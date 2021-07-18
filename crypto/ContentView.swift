8b379cb9c90e767b2a497a80852c7ca38e0cbf77
lukaszlukaszkk <lukasz.luczak12@gmail.com>
2021-07-18 16:34:02 +0200

    init

---
+++ crypto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  crypto
+//
+//  Created by Łukasz Łuczak on 18/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ crypto/cryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoApp.swift
+//  crypto
+//
+//  Created by Łukasz Łuczak on 18/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct cryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
