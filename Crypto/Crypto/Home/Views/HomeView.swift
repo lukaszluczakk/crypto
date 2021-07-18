cba0770bfb01d50392889fd730e570179a4cab5c
lukaszlukaszkk <lukasz.luczak12@gmail.com>
2021-07-18 17:25:48 +0200

    HomeView, Color extension

---
+++ Crypto/Crypto/Home/Views/HomeView.swift
@@ -0,0 +1,31 @@
+//
+//  HomeView.swift
+//  Crypto
+//
+//  Created by Łukasz Łuczak on 18/07/2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Home")
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeView()
+                .navigationBarHidden(true)
+        }
+    }
+}
