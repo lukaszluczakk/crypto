3f977c84bfe49947dae53d13499cc2ad6770c8af
lukaszlukaszkk <lukasz.luczak12@gmail.com>
2021-07-20 22:13:00 +0200

    CoinRowView, extensions

--- Crypto/Crypto/Components/CircleButton/CIrcleButtonAnimationView.swift
+++ Crypto/Crypto/Components/CircleButton/CIrcleButtonAnimationView.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct CIrcleButtonAnimationView: View {
+    @Binding var animate : Bool
+    
     var body: some View {
         Circle()
-            .stroke()
+            .stroke(lineWidth: 5.0)
+            .scale(animate ? 1.0 : 0.0)
+            .opacity(animate ? 0.0 : 1.0 )
+            .animation(animate ? Animation.easeOut(duration: 1.0) : .none)
     }
 }
 
 struct CIrcleButtonAnimationView_Previews: PreviewProvider {
     static var previews: some View {
-        CIrcleButtonAnimationView()
+        CIrcleButtonAnimationView(animate: .constant(false))
+            .foregroundColor(Color.red)
+            .frame(width: 100, height: 100)
     }
 }
--- Crypto/Crypto/Home/Views/HomeView.swift
+++ Crypto/Crypto/Home/Views/HomeView.swift
@@ -16,30 +16,13 @@ struct HomeView: View {
                 .ignoresSafeArea()
             
             VStack {
-                HStack {
-                    CircleButtonView(iconName: showPortfolio ? "plus" : "info")
-                        .animation(.none)
-                    Spacer()
-                    Text(showPortfolio ? "Portfolio" : "Live prices")
-                        .font(.headline)
-                        .fontWeight(.heavy)
-                        .foregroundColor(Color.theme.accent)
-                        .animation(.none)
-                    Spacer()
-                    CircleButtonView(iconName: "chevron.right")
-                        .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
-                        .onTapGesture {
-                            withAnimation(.spring()) {
-                                showPortfolio.toggle()
-                            }
-                        }
-                    }
-                }.padding(.horizontal)
-                
+                homeHeader
                 Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
+                
             }
         }
     }
+}
 
 
 struct HomeView_Previews: PreviewProvider {
@@ -48,5 +31,31 @@ struct HomeView_Previews: PreviewProvider {
             HomeView()
                 .navigationBarHidden(true)
         }
+    }
+}
+
+extension HomeView {
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none)
+                .background (
+                    CIrcleButtonAnimationView(animate: $showPortfolio)
+                )
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }.padding(.horizontal)
     }
 }
