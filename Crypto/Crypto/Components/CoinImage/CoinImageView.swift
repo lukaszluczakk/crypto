613c77f237e60bc40e28a59bc122bc0f637c1b15
lukaszlukaszkk <lukasz.luczak12@gmail.com>
2021-07-23 23:45:18 +0200

    coin image

---
+++ Crypto/Crypto/Components/CoinImage/CoinImageView.swift
@@ -0,0 +1,47 @@
+//
+//  CoinImageView.swift
+//  Crypto
+//
+//  Created by Łukasz Łuczak on 23/07/2021.
+//
+
+import SwiftUI
+
+class CoinImageViewModel: ObservableObject  {
+    @Published var image: UIImage? = nil
+    @Published var isLoading: Bool = true
+    
+    init() {
+        
+    }
+    
+    private func getImage(){
+        
+    }
+}
+
+struct CoinImageView: View {
+    @StateObject var vm: CoinImageViewModel = CoinImageViewModel()
+    var body: some View {
+        ZStack {
+            if let image = vm.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            } else if vm.isLoading {
+                ProgressView()
+            } else {
+                Image(systemName: "questionmark")
+                    .foregroundColor(Color.theme.secondaryText)
+            }
+        }
+    }
+}
+
+struct CoinImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinImageView()
+            .padding()
+            .previewLayout(.sizeThatFits)
+    }
+}
